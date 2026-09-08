c9d00a07b9bf0f229bb6fb937146dbdd95c9fc7b
Mudassar ijaz <24abswe0001@uetpeshawar.edu.pk>
2026-09-08 23:49:14 +0500

    Update Lab04task02.java

--- lab04task02/src/main/java/com/university/lab/lab04task02/Lab04task02.java
+++ lab04task02/src/main/java/com/university/lab/lab04task02/Lab04task02.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.university.lab.lab04task02;
-
-/**
- *
- * @author mudas
- */
-public class Lab04task02 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
