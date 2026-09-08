c9030ac73a68ff4a4cfaed3bfe192d615e538082
Mudassar ijaz <24abswe0001@uetpeshawar.edu.pk>
2026-09-08 23:47:22 +0500

    Update Lab04homework01.java

--- lab04homework01/src/main/java/com/university/lab/lab04homework01/Lab04homework01.java
+++ lab04homework01/src/main/java/com/university/lab/lab04homework01/Lab04homework01.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.university.lab.lab04homework01;
-
-/**
- *
- * @author mudas
- */
-public class Lab04homework01 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
